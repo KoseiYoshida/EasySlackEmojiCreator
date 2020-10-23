3a863f07f8011800191d43b4e7cc626db66d0aee
KoseiYoshida <yoshida.kosei@synamon.jp>
2020-10-23 11:49:46 +0900

    fix 不要なusingを削除した

--- SlackEmojiCreator/SelectableEmojiData.cs
+++ SlackEmojiCreator/SelectableEmojiData.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
 using System.Windows;
-using System.Windows.Media;
 
 namespace SlackEmojiCreator
 {
